429fe9696abb695d9b0aca0f9dbeab3dbf74083a
Pedro Igor <40576393+pmalaquias@users.noreply.github.com>
2022-12-30 12:10:39 -0300

    Add color scheme

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'color_schemes.g.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -10,10 +12,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
         useMaterial3: true,
+        colorScheme: lightColorScheme,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
